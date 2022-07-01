3da88d401b60856cc7d6fb3db95a16af61015ee1
Pawel Mechlinski <s26129@pjwstk.edu.pl>
2022-07-01 09:45:30 +0200

    I've refactored Cell.class: - deleted unused fields and methods. - Added linkedObject to link with game objects on a given field. - toString gets sprite from gameObject - (Old toString is here to stay until I migrate sprites to a new file).

--- Tankz/src/Cell.java
+++ Tankz/src/Cell.java
@@ -6,82 +6,57 @@ import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.JLabel;
 // Render + logika + klasa-tablica z polami gry i ich wartościami
 @SuppressWarnings("serial")
-public class Cell extends JLabel {
+public class Cell extends JLabel
+{
 	Cell[][] neibers = new Cell[3][3];
-	public boolean isPlayer () { if (this.objectType == 3) return true; return false; };
-
-	public int hp;
-	public Cell movable (int kierunek) // TODO: Change into probably enum with used keys
+	private GameObject linkedObject;
+	public void setNeiber(Cell cell, int p, int q)
 	{
-		setHorizontalTextPosition(CENTER);
-		Cell movable;
-		switch (kierunek) {
-			case 1: //góra
-				movable = neibers[2][1];
-				break;
-			case 2: //dół
-				movable = neibers[2][3];
-				break;
-			case 3: //lewo
-				movable = neibers[1][2];
-				break;
-			case 4: //prawo
-				movable = neibers[3][2];
-				break;
-			default:
-				movable = neibers[2][2];
-				break;
-		}
-		return movable;
-	}
-	private int objectType; // { 1:Wall, 2:Base, 3:Player, 4:Enemy, 5:MissileH, 6:MissileV };
-
-//	public Cell() {
-//		setHorizontalTextPosition(CENTER);
-//	}
-
-	public void setNeiber(Cell cell, int p, int q) {
 		neibers[p][q] = cell;
 	}
 
 	@Override
-	public String toString() {
-		//1:Wall, 2:Base, 3:Player, 4:Enemy, 5:MissileH, 6:MissileV
-		// Using a pinch of html in JLabels can make every gameObject have 3x3 dimension.
-		String sprite;
-		switch(objectType) {
-			case 1:
-				sprite = "<html>###<br/>###<br/>###</html>";
-				break;
-			case 2:
-				sprite = "<html>@=@<br/>|| F ||<br/>@=@</html>";
-				break;
-			case 3:
-				sprite = "<html>_|_<br/>[+]<br/></html>";
-				break;
-			case 4:
-				sprite = "<html>_!_<br/>[X]<br/>###</html>";
-				break;
-			case 5:
-				sprite = "*";
-				break;
-			default:
-				sprite = "";
-				break;
-		}
-		return sprite;
-	}
-
-	public boolean isEntity ()
+	public String toString()
 	{
-		if (this.toString() == "") return false;
-		else return true;
+		if (linkedObject != null)
+		{
+			return linkedObject.getImage();
+		}
+		return "";
 	}
 
+//		//1:Wall, 2:Base, 3:Player, 4:Enemy, 5:MissileH, 6:MissileV
+//		// Using a pinch of html in JLabels can make every gameObject have 3x3 dimension.
+//		String sprite;
+//		switch(objectType) {
+//			case 1:
+//				sprite = "<html>###<br/>###<br/>###</html>";
+//				break;
+//			case 2:
+//				sprite = "<html>@=@<br/>|| F ||<br/>@=@</html>";
+//				break;
+//			case 3:
+//				sprite = "<html>_|_<br/>[+]<br/></html>";
+//				break;
+//			case 4:
+//				sprite = "<html>_!_<br/>[X]<br/>###</html>";
+//				break;
+//			case 5:
+//				sprite = "*";
+//				break;
+//			default:
+//				sprite = "";
+//				break;
+//		}
+//		return sprite;
+//	}
 
-	public void setEntity(int entityNumber) { //Default Set entity function.
-		this.objectType = entityNumber;
-		toString();
+	public GameObject getLinkedObject() {
+		return linkedObject;
+	}
+
+	public void setLinkedObject(GameObject linkedObject) {
+		this.linkedObject = linkedObject;
 	}
 
 	void redraw() {
